efa0a4e57b2bfd7434840b0ce99aa02c8c35d45f
Aminulloh Zaqi <aminulloh.zaqi@gmail.com>
2024-01-29 10:05:36 +0700

    chnage sidebar menu by role

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -1,7 +1,43 @@
 <template>	
-	<sidebar-menu 
+	<sidebar-menu v-if="role==='Administrator'"
 		@update:collapsed="onToggleCollapse"
-		:menu="menu"
+		:menu="menuAdministrator"
+		:showOneChild="showOneChild"
+		:width="width">
+		<template #header>
+            <img
+                v-if="!sidebarCollapsed"
+                src="@/assets/logo-simanja-white.svg"
+                class="d-block org-logo"
+            >
+            <img
+                v-if="sidebarCollapsed"
+                src="@/assets/logo-BMKG.png"
+                class="d-block org-logo-collapsed"
+            >
+        </template>
+	</sidebar-menu>
+	<sidebar-menu v-else-if="role==='Verifikator'"
+		@update:collapsed="onToggleCollapse"
+		:menu="menuVerifikator"
+		:showOneChild="showOneChild"
+		:width="width">
+		<template #header>
+            <img
+                v-if="!sidebarCollapsed"
+                src="@/assets/logo-simanja-white.svg"
+                class="d-block org-logo"
+            >
+            <img
+                v-if="sidebarCollapsed"
+                src="@/assets/logo-BMKG.png"
+                class="d-block org-logo-collapsed"
+            >
+        </template>
+	</sidebar-menu>
+	<sidebar-menu v-else-if="role==='User'"
+		@update:collapsed="onToggleCollapse"
+		:menu="menuUser"
 		:showOneChild="showOneChild"
 		:width="width">
 		<template #header>
@@ -29,11 +65,12 @@ export default {
 	},
 	data() {
 		return {
+            role: localStorage.getItem('role'),
 			relative: true,
 			sidebarCollapsed: false,
 			showOneChild: true,
 			width: '260px',
-			menu: [
+			menuAdministrator: [
 				{
 					href: '/dashboard',
 					title: 'Dashboard',
@@ -148,7 +185,148 @@ export default {
 						}
 					]
 				},
-			]
+			],
+			menuVerifikator: [
+				{
+					href: '/dashboard',
+					title: 'Dashboard',
+					icon: 'fa fa-bar-chart'
+				},
+				{
+					title: 'Data Master',
+					icon: 'fa fa-book',
+					child: [
+					{
+							href: '/jabatan',
+							title: 'Jabatan'
+						},
+						{
+							href: '/satker',
+							title: 'Unit Kerja'
+						},
+						{
+							href: '/ikhtisar-jabatan',
+							title: 'Ikhtisar Jabatan'
+						},
+						{
+							href: '/tugas-pokok',
+							title: 'Tugas Pokok'
+						},
+						{
+							href: '/pendidikan-formal',
+							title: 'Pendidikan Formal'
+						},
+						{
+							href: '/diklat',
+							title: 'Pendidikan dan Pelatihan'
+						},
+						{
+							href: '/pengalaman-kerja',
+							title: 'Pengalaman Kerja'
+						},
+						{
+							href: '/bahan-kerja',
+							title: 'Bahan kerja'
+						},
+						{
+							href: '/perangkat-kerja',
+							title: 'Perangkat Kerja'
+						},
+						{
+							href: '/tanggung-jawab',
+							title: 'Tanggung Jawab'
+						},
+						{
+							href: '/wewenang',
+							title: 'Wewenang'
+						},
+						{
+							href: '/bakat-kerja',
+							title: 'Bakat Kerja'
+						},
+						{
+							href: '/temperamen-kerja',
+							title: 'Temperamen Kerja'
+						},
+						{
+							href: '/minat-kerja',
+							title: 'Minat Kerja'
+						},
+						{
+							href: '/fungsi-pekerjaan',
+							title: 'Fungsi Pekerjaan'
+						},
+					]
+				},
+				{
+					title: 'Analisis Jabatan',
+					icon: 'fa fa-pencil-square-o',
+					child: [
+						{
+							href: '/data-jabatan',
+							title: 'Data Jabatan'
+						},
+						{
+							href: '/analisis-jabatan',
+							title: 'Analisis Jabatan'
+						},
+					]
+				},
+				{
+					href: '/verifikasi',
+					title: 'Verifikasi',
+					icon: 'fa fa-check-square-o'
+				},
+				{
+					title: 'Report',
+					icon: 'fa fa-file-text',
+					child: [
+						{
+							href: '/rekapitulasi-jabatan',
+							title: 'Rekapitulasi Jabatan'
+						},
+						{
+							href: '/cetak-laporan',
+							title: 'Cetak Laporan'
+						}
+					]
+				}
+			],
+			menuUser: [
+				{
+					href: '/dashboard',
+					title: 'Dashboard',
+					icon: 'fa fa-bar-chart'
+				},
+				{
+					title: 'Analisis Jabatan',
+					icon: 'fa fa-pencil-square-o',
+					child: [
+						{
+							href: '/data-jabatan',
+							title: 'Data Jabatan'
+						},
+						{
+							href: '/analisis-jabatan',
+							title: 'Analisis Jabatan'
+						},
+					]
+				},
+				{
+					title: 'Report',
+					icon: 'fa fa-file-text',
+					child: [
+						{
+							href: '/rekapitulasi-jabatan',
+							title: 'Rekapitulasi Jabatan'
+						},
+						{
+							href: '/cetak-laporan',
+							title: 'Cetak Laporan'
+						}
+					]
+				}
+			],
 		}
     },
 	methods: {
